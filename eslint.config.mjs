09aa31baaae2887b181114c1f0ff04fc1a6e7f9e
Shailesh Rajesh M <156943530+ShaileshRajesh@users.noreply.github.com>
2026-05-07 18:30:20 +0000

    initial commit to husky integration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,39 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  prettier,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+
+    // Ignore config files
+    '*.config.js',
+    '*.config.mjs',
+    '.lintstagedrc.js',
+    'commitlint.config.js',
   ]),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
