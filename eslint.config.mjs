a83c55c369af9bbeae367fcdecb2fbd8a423c026
Shailesh Rajesh M <156943530+ShaileshRajesh@users.noreply.github.com>
2026-05-08 13:20:27 +0000

    implemented plop for component creation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,6 +26,7 @@ const eslintConfig = defineConfig([
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-vars': [
         'warn',
         {
