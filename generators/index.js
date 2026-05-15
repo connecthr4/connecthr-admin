5e6fccd72dc3fdcb961514c2550f54c02ab59aac
Shailesh Rajesh M <156943530+ShaileshRajesh@users.noreply.github.com>
2026-05-15 12:33:09 +0000

    added plop files for route creation

--- generators/index.js
+++ generators/index.js
@@ -1,6 +1,6 @@
 module.exports = function (plop) {
   require('./component/index')(plop);
-  //   require('./constant/index')(plop);
+  require('./route/index')(plop);
   //   require('./utility/index')(plop);
   //   require('./store/index')(plop);
   //add the new generate file path
