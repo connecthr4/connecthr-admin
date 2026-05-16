2f73bd5ffbf027bb8eeab0074caea2395a042c64
Shailesh Rajesh M <156943530+ShaileshRajesh@users.noreply.github.com>
2026-05-16 17:16:53 +0000

    added pie chart base component

---
+++ src/components/BasePieChart/BasePieChart.tsx
@@ -0,0 +1,67 @@
+/**
+ * Reusable base component for rendering responsive pie and donut charts across the application.
+ *
+ * @example
+ * ```tsx
+ * import BasePieChart from '@src/components/BasePieChart'
+ *
+ * export default function BasePieChart() {
+ *   return <BasePieChart label="Hello" />;
+ * }
+ * ```
+ */
+'use client';
+
+import { Cell, Legend, Pie, PieChart, Tooltip } from 'recharts';
+import ChartContainer from '../ChartContainer';
+import styles from './BasePieChart.module.scss';
+
+type PieChartData = {
+  name: string;
+  value: number;
+  color: string;
+};
+
+/**
+ * Define the props available for the BasePieChart component.
+ */
+interface BasePieChartProps {
+  data: PieChartData[];
+  height?: number;
+  innerRadius?: number | string;
+  outerRadius?: number | string;
+  showLegend?: boolean;
+  isAnimationActive?: boolean;
+}
+
+export default function BasePieChart({
+  data,
+  height = 500,
+  innerRadius = '80%',
+  outerRadius = '100%',
+  showLegend = true,
+  isAnimationActive = true,
+}: BasePieChartProps) {
+  return (
+    <ChartContainer height={height}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          nameKey="name"
+          outerRadius={outerRadius}
+          innerRadius={innerRadius}
+          isAnimationActive={isAnimationActive}
+        >
+          {data.map((entry) => (
+            <Cell key={entry.name} fill={entry.color} />
+          ))}
+        </Pie>
+
+        <Tooltip />
+
+        {showLegend && <Legend verticalAlign="bottom" align="center" iconType="rect" />}
+      </PieChart>
+    </ChartContainer>
+  );
+}
